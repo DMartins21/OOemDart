4658323e5a0316ca9ac4855b40ca1f2f2f7b454c
Davi Martins <davi_martins2009outlook.com>
2024-02-26 19:00:17 -0300

    Sobrescrita, Construtor e Encapsulamento

---
+++ bin/app.dart
@@ -0,0 +1,8 @@
+import 'package:app/Entities/Pessoa.dart';
+
+void main(List<String> arguments) {
+  var p1 = new Pessoa("Davi", "Rua 20");
+  //p1.setNome("Davi");
+  //p1.setEndereco("Rua 30");
+  print(p1);
+}
---
+++ lib/Entities/Pessoa.dart
@@ -0,0 +1,31 @@
+class Pessoa {
+
+  String _nome = "";
+  String _endereco = "";
+
+  Pessoa(String nome, String endereco){
+    _nome = nome;
+    _endereco = endereco;
+  }
+
+  void setNome(String nome){
+    _nome = nome;
+  }
+
+  String getNome(){
+    return _nome;
+  }
+  
+  void setEndereco(String endereco){
+    _endereco = endereco;
+  }
+
+    String getEndereco(){
+    return _endereco;
+  }
+
+  @override
+  String toString(){
+    return{"Nome": _nome, "Endereco": _endereco}.toString();
+  }
+}
